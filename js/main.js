1a346992d9d4ebc8d7bfab9860a3d096f500514c
Eugene <zheka20042006@gmail.com>
2021-02-11 18:46:08 +1000

    All blocks, smooth scrolling, and form submission checks are done.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
 $(document).ready(function () {
-   // headerBurger event "onclick"
-   $('.header__burger').click(function (e) { 
-      $('.header__burger, .header__menu').toggleClass('active');
-      $('body').toggleClass('lock');
-   });
+  // headerBurger event "onclick"
+  $(".header__burger").click(function (e) {
+    $(".header__burger, .header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
 
-   $('.slider').slick({
-      arrows: false,
-      dots: true,
-      adaptiveHeight: true,
-      speed: 1000,
-      cssEase: "ease",
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 5000,
-      draggable: false, 
-      swipe: false,
-      fade: true,
-      pauseOnHover: false, 
-      pauseOnFocus: false,
-      responsive: [
-         {
-            breakpoint: 575,
-            settings: {
-               dots: false,
-            }
-         }
-      ]
-   });
+  // Slick Slider Settings
+  $(".slider").slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    speed: 1000,
+    cssEase: "ease",
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    draggable: false,
+    swipe: false,
+    fade: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          dots: false,
+        },
+      },
+    ],
+  });
+
+  // Slow Scroll from btn to form
+  $("a.btn").click(function () {
+    let formHeight = $(".header").height() * 1.2;
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - formHeight + "px",
+      },
+      {
+        duration: 1000,
+        easing: "swing",
+      }
+    );
+    return false;
+  });
+
+  //Check checkbox status
+  $("#form__checkbox").on("change", function () {
+    if ($("#form__checkbox").prop("checked")) {
+      $(".form__btn").removeClass("disabled");
+    } else {
+      $(".form__btn").addClass("disabled");
+    }
+  });
 });
