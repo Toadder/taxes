792037250c2686c4fd54f9ee2a9ed59da9da2811
Eugene <zheka20042006@gmail.com>
2021-02-13 19:28:36 +1000

    ~Popup is done! The website is done!~

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Jquery
 $(document).ready(function () {
   // headerBurger event "onclick"
   $(".header__burger").click(function (e) {
@@ -14,38 +15,30 @@ $(document).ready(function () {
     cssEase: "ease",
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 5000,
-    draggable: false,
-    swipe: false,
+    autoplaySpeed: 4000,
+    draggable: true,
+    swipe: true,
     fade: true,
     pauseOnHover: false,
     pauseOnFocus: false,
-    responsive: [
-      {
-        breakpoint: 575,
-        settings: {
-          dots: false,
-        },
-      },
-    ],
   });
 
-  // Slow Scroll from btn to form
-  $("a.btn").click(function () {
-    let formHeight = $(".header").height() * 1.2;
-    $("html, body").animate(
-      {
-        scrollTop: $($(this).attr("href")).offset().top - formHeight + "px",
-      },
-      {
-        duration: 1000,
-        easing: "swing",
-      }
-    );
-    return false;
-  });
+  // // Slow Scroll from btn to form
+  // $("a.btn").click(function () {
+  //   let formHeight = $(".header").height() * 1.2;
+  //   $("html, body").animate(
+  //     {
+  //       scrollTop: $($(this).attr("href")).offset().top - formHeight + "px",
+  //     },
+  //     {
+  //       duration: 1000,
+  //       easing: "swing",
+  //     }
+  //   );
+  //   return false;
+  // });
 
-  //Check checkbox status
+  //Check checkbox status in the page (section-three)
   $("#form__checkbox").on("change", function () {
     if ($("#form__checkbox").prop("checked")) {
       $(".form__btn").removeClass("disabled");
@@ -53,4 +46,116 @@ $(document).ready(function () {
       $(".form__btn").addClass("disabled");
     }
   });
+
+  // Check checkbox status the popup
+  $("#popup-form__checkbox").on("change", function () {
+    if ($("#popup-form__checkbox").prop("checked")) {
+      $(".popup-form__btn").removeClass("disabled");
+    } else {
+      $(".popup-form__btn").addClass("disabled");
+    }
+  });
 });
+
+// Native JS
+// Popups
+(function () {
+  const popupLinks = document.querySelectorAll(".popup-link");
+  const body = document.querySelector("body");
+  const lockPadding = document.querySelector(".lock-padding");
+
+  let unlock = true;
+
+  const timeout = 500;
+
+  if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+      const popupLink = popupLinks[index];
+      popupLink.addEventListener("click", function (e) {
+        const popupName = popupLink.getAttribute("href").replace("#", "");
+        const currentPopup = document.getElementById(popupName);
+        popupOpen(currentPopup);
+        e.preventDefault();
+      });
+    }
+  }
+
+  const popupCloseIcon = document.querySelectorAll(".close-popup");
+  if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+      const el = popupCloseIcon[index];
+      el.addEventListener("click", function (e) {
+        popupClose(el.closest(".popup"));
+        e.preventDefault();
+      });
+    }
+  }
+
+  function popupOpen(currentPopup) {
+    if (currentPopup && unlock) {
+      const popupActive = document.querySelector(".popup.open");
+      if (popupActive) {
+        popupClose(popupActive, false);
+      } else {
+        bodyLock();
+      }
+      currentPopup.classList.add("open");
+      currentPopup.addEventListener("click", function (e) {
+        if (!e.target.closest(".popup__content")) {
+          popupClose(e.target.closest(".popup"));
+        }
+      });
+    }
+  }
+
+  function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+      popupActive.classList.remove("open");
+      if (doUnlock) {
+        bodyUnlock();
+      }
+    }
+  }
+
+  function bodyLock() {
+    const lockPaddingValue =
+      window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+
+    if (lockPadding.length > 0) {
+      for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index];
+        el.style.paddingRight = lockPaddingValue;
+      }
+    }
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add("lock");
+
+    unlock = false;
+    setTimeout(function () {
+      unlock = true;
+    }, timeout);
+  }
+
+  function bodyUnlock() {
+    setTimeout(function () {
+      for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index];
+        el.style.paddingRight = "0px";
+      }
+      body.classList.remove("lock");
+      body.style.paddingRight = "0px";
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function () {
+      unlock = true;
+    }, timeout);
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.which == 27) {
+      const popupActive = document.querySelector(".popup");
+      popupClose(popupActive);
+    }
+  });
+})();
